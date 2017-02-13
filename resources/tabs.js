bbef785e3008fda392c67e568f10ae70ed737ad1
Eitan Suez <eitan.suez@gmail.com>
2017-02-13 09:58:18 -0600

    resolved tabs logic to handle more than one set of tabs on a page

--- resources/tabs.js
+++ resources/tabs.js
@@ -1,6 +1,10 @@
 $(function() {
-  var classname = $(".tabs").data("for");
-  var stuff = $("."+classname).detach();
-  $(".tabs").append(stuff);
-  $(".tabs").tabs();
+  var tabs = $(".tabs");
+  $.each(tabs, function() {
+    var tabElement = $(this);
+    var tabname = tabElement.data("for");
+    var tabcontents = $("."+tabname).detach();
+    tabElement.append(tabcontents);
+  });
+  tabs.tabs();
 });
